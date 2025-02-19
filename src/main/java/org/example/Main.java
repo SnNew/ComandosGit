d5a01948b8193e74b3600577502a4ebb18e31afb
Santiago <new.noguera@gmail.com>
2025-02-19 13:06:55 -0500

    mas salidas por consola

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
+        system.out.println("Santiago");
+        system.out.println("Santiago");
+        system.out.println("Santiago");
+        system.out.println("Santiago");
+        system.out.println("Santiago");
+        system.out.println("Santiago");
     }
 }
